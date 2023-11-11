2345444975fe03002cbc97b9558eac1b80c3e33b
Iván García Auteville <ivangarcia96@hotmail.es>
2023-11-11 04:54:42 -0300

    GameMode Spawns the Pawn
    Using PlayerSpawn and GameMode to Spawn the Pawn. Same for Invaders in next iterations.

--- Source/SpaceInvaders/GameModes/ASIGameModeBase.cpp
+++ Source/SpaceInvaders/GameModes/ASIGameModeBase.cpp
@@ -3,3 +3,51 @@
 
 #include "ASIGameModeBase.h"
 
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "SpaceInvaders/Actors/SIPlayerSpawner.h"
+#include "SpaceInvaders/Actors/SIInvadersSpawner.h"
+#include "SpaceInvaders/Player/ASIPlayerPawn.h"
+#include "SpaceInvaders/Player/ASIPlayerController.h"
+
+// TODO: Move to Utils
+namespace GameModeUtils
+{
+	template<typename Type>
+	inline Type* FindSingleActor(const UWorld& World, const TSubclassOf<Type> ActorClass)
+	{
+		TArray<AActor*> FoundActors;
+		UGameplayStatics::GetAllActorsOfClass(&World, ActorClass, FoundActors);
+
+		// We expect only one of this type
+		if (FoundActors.Num() != 1)
+			return nullptr;
+
+		return CastChecked<Type>(FoundActors[0]);
+	}
+}
+
+void AASIGameModeBase::BeginPlay()
+{
+	UWorld* World = GetWorld();
+
+	PlayerSpawner = GameModeUtils::FindSingleActor(*World, PlayerSpawnerClass);
+	InvadersSpawner = GameModeUtils::FindSingleActor(*World, InvadersSpawnerClass);
+	PlayerController = CastChecked<AASIPlayerController>(UGameplayStatics::GetPlayerController(World, 0));
+
+	SpawnPawn(World);
+
+	PlayerController->Possess(Pawn);
+}
+
+void AASIGameModeBase::SpawnPawn(UWorld* World)
+{
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	FVector SpawnLocation = PlayerSpawner->GetActorLocation();
+	FRotator SpawnRotation = PlayerSpawner->GetActorRotation();
+
+	Pawn = World->SpawnActor<AASIPlayerPawn>(PlayerPawnClass, SpawnLocation, SpawnRotation, SpawnParams);
+}
--- Source/SpaceInvaders/GameModes/ASIGameModeBase.h
+++ Source/SpaceInvaders/GameModes/ASIGameModeBase.h
@@ -6,6 +6,11 @@
 #include "GameFramework/GameModeBase.h"
 #include "ASIGameModeBase.generated.h"
 
+class ASIPlayerSpawner;
+class ASIInvadersSpawner;
+class AASIPlayerPawn;
+class AASIPlayerController;
+
 /**
  * 
  */
@@ -13,5 +18,31 @@ UCLASS()
 class SPACEINVADERS_API AASIGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override final;
+
+	void SpawnPawn(UWorld* World);
+
+private:
+	UPROPERTY(Category = "Spawners", EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	TSubclassOf<ASIPlayerSpawner> PlayerSpawnerClass = nullptr;
+
+	UPROPERTY(Category = "Spawners", EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	TSubclassOf<ASIInvadersSpawner> InvadersSpawnerClass = nullptr;
+
+	UPROPERTY(Category = "PlayerPawn", EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	TSubclassOf<AASIPlayerPawn> PlayerPawnClass = nullptr;
+
+	UPROPERTY(Category = "Instances", VisibleAnywhere, meta = (AllowPrivateAccess = true))
+	ASIPlayerSpawner* PlayerSpawner = nullptr;
+
+	UPROPERTY(Category = "Instances", VisibleAnywhere, meta = (AllowPrivateAccess = true))
+	ASIInvadersSpawner* InvadersSpawner = nullptr;
 	
+	UPROPERTY(Category = "Instances", VisibleAnywhere, meta = (AllowPrivateAccess = true))
+	AASIPlayerController* PlayerController = nullptr;
+
+	UPROPERTY(Category = "Instances", VisibleAnywhere, meta = (AllowPrivateAccess = true))
+	AASIPlayerPawn* Pawn = nullptr;
 };
