2f347a2db8f361ca03fd2980c51d81fc196b6be8
Iván García Auteville <ivangarcia96@hotmail.es>
2023-11-12 00:10:28 -0300

    Updated UFO Actor
    Added more functionality

--- Source/SpaceInvaders/Enemies/ASIUFOActor.cpp
+++ Source/SpaceInvaders/Enemies/ASIUFOActor.cpp
@@ -2,20 +2,23 @@
 
 
 #include "ASIUFOActor.h"
-#include "Components/SceneComponent.h"
+//#include "Components/SceneComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 AASIUFOActor::AASIUFOActor()
 {
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
-	SceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
-	SceneComp->SetMobility(EComponentMobility::Movable);
-	SetRootComponent(SceneComp);
+	//SceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
+	//SceneComp->SetMobility(EComponentMobility::Movable);
+	//SetRootComponent(SceneComp);
 
 	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
 	StaticMeshComp->SetMobility(EComponentMobility::Movable);
-	StaticMeshComp->SetupAttachment(RootComponent);
+	//StaticMeshComp->SetupAttachment(RootComponent);
+	SetRootComponent(StaticMeshComp);
 
 	StaticMeshComp->SetGenerateOverlapEvents(false);
 	StaticMeshComp->SetNotifyRigidBodyCollision(false);
@@ -26,19 +29,77 @@ AASIUFOActor::AASIUFOActor()
 	// Custom Preset
 	// Collision Enabled (Query Only)
 	// Object Type: WorldDynamic
-	// Ignore everything, Block WoldDynamic (Projectiles)
+	// Ignore everything, Block WoldDynamic (Projectiles) and WorldStatic (bounds)
 
+	AudioComp = CreateDefaultSubobject<UAudioComponent>(TEXT("Audio"));
+	AudioComp->SetupAttachment(RootComponent);
+	AudioComp->SetVolumeMultiplier(1.0f);
+	AudioComp->SetPitchMultiplier(1.0f);
 }
 
-//void AASIUFOActor::BeginPlay()
-//{
-//	Super::BeginPlay();
-//	
-//}
+void AASIUFOActor::SetHorizontalMovementDirection(const EHorizontalMovementType MovementDirection)
+{
+	HorizontalMovementType = MovementDirection;
+}
 
-//void AASIUFOActor::Tick(float DeltaTime)
+void AASIUFOActor::HandleDestruction()
+{
+	// TODO: VFX
+	//UGameplayStatics::SpawnEmitterAtLocation(this, DeathParticles, GetActorLocation(), GetActorRotation());
+	
+	// TODO: Sound
+	//UGameplayStatics::PlaySoundAtLocation(this, DeathSound, GetActorLocation());
+
+	Destroy();
+}
+
+void AASIUFOActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//StaticMeshComp->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
+
+	bEnabled = true;
+
+	AudioComp->Play();
+}
+
+void AASIUFOActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!bEnabled)
+		return;
+	
+	Move(DeltaTime);
+}
+
+void AASIUFOActor::Move(float DeltaTime)
+{
+	const float Direction = (HorizontalMovementType == EHorizontalMovementType::Right) ? 1.0f : -1.0f;
+	FVector LocalOffset(Direction * MovementSpeed * DeltaTime, 0.0, 0.0);
+	AddActorLocalOffset(LocalOffset, true);
+}
+
+//void AASIUFOActor::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
+//	FVector NormalImpulse, const FHitResult& Hit)
 //{
-//	Super::Tick(DeltaTime);
+//	if (OtherActor == nullptr || OtherActor == this)
+//		return;
 //
+//	// TODO: do something with OtherActor
+//	UE_LOG(LogTemp, Warning, TEXT("UFO Collision with %s"), *OtherActor->GetName());
+//
+//	// TODO: VFX
+//	//UGameplayStatics::SpawnEmitterAtLocation(this, HitParticles, GetActorLocation(), GetActorRotation());
+//
+//	// TODO: Sound
+//	//UGameplayStatics::PlaySoundAtLocation(this, HitSound, GetActorLocation());
+//
+//	Disable();
+//
+//	if (!OnProjectileHit.IsBound())
+//		return;
+//
+//	OnProjectileHit.Broadcast();
 //}
-
--- Source/SpaceInvaders/Enemies/ASIUFOActor.h
+++ Source/SpaceInvaders/Enemies/ASIUFOActor.h
@@ -4,30 +4,53 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "SpaceInvaders/Enums/HorizontalMovementType.h"
 #include "ASIUFOActor.generated.h"
 
-class USceneComponent;
 class UStaticMeshComponent;
+class UAudioComponent;
 
-UCLASS()
+//DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnUFODestroyedByPlayer);
+
+UCLASS(meta = (PrioritizeCategories = "UFOSettings UFOInstance Components"))
 class SPACEINVADERS_API AASIUFOActor : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	AASIUFOActor();
+	void Tick(float DeltaTime) override final;
+	void SetHorizontalMovementDirection(const EHorizontalMovementType MovementDirection);
 
-//protected:
-//	virtual void BeginPlay() override;
-//
-//public:	
-//	virtual void Tick(float DeltaTime) override;
+	void HandleDestruction();
+//public:
+//	// Delegates
+//	UPROPERTY(BlueprintAssignable)
+//	FOnUFODestroyedByPlayer OnUFODestroyedByPlayer;
+
+protected:
+	void BeginPlay() override final;
 
 private:
-	UPROPERTY(Category = "Component", VisibleAnywhere, meta = (AllowPrivateAccess = true))
-	TObjectPtr<USceneComponent> SceneComp = nullptr;
+	//UFUNCTION()
+	//void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	//		FVector NormalImpulse, const FHitResult& Hit);
 
+	void Move(float DeltaTime);
+
+private:
 	UPROPERTY(Category = "Components", VisibleAnywhere, meta = (AllowPrivateAccess = true))
 	TObjectPtr<UStaticMeshComponent> StaticMeshComp = nullptr;
 
+	UPROPERTY(Category = "Components", VisibleAnywhere, meta = (AllowPrivateAccess = true))
+	TObjectPtr<UAudioComponent> AudioComp = nullptr;
+
+	UPROPERTY(Category = "UFOSettings", EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	float MovementSpeed = 200.0f;
+
+	UPROPERTY(Category = "UFOInstance", VisibleAnywhere, meta = (AllowPrivateAccess = true))
+	EHorizontalMovementType HorizontalMovementType = EHorizontalMovementType::None;
+
+	UPROPERTY(Category = "UFOInstance", VisibleAnywhere, meta = (AllowPrivateAccess = true))
+	bool bEnabled = false;
 };
