9b24ac47fcf9a32240d7f01ed1b55947b209f155
Iván García Auteville <ivangarcia96@hotmail.es>
2023-11-13 03:53:33 -0300

    GameInstance almost ready

--- Source/SpaceInvaders/GameInstances/USIGameInstance.cpp
+++ Source/SpaceInvaders/GameInstances/USIGameInstance.cpp
@@ -3,3 +3,26 @@
 
 #include "USIGameInstance.h"
 
+void UUSIGameInstance::Init()
+{
+	Super::Init();
+	LoadGameData();
+}
+
+void UUSIGameInstance::Shutdown()
+{
+	Super::Shutdown();
+	SaveGameData();
+}
+
+bool UUSIGameInstance::LoadGameData()
+{
+
+	return true;
+}
+
+bool UUSIGameInstance::SaveGameData()
+{
+
+	return true;
+}
--- Source/SpaceInvaders/GameInstances/USIGameInstance.h
+++ Source/SpaceInvaders/GameInstances/USIGameInstance.h
@@ -13,17 +13,42 @@ class SPACEINVADERS_API UUSIGameInstance : public UGameInstance
 	GENERATED_BODY()
 
 public:
+	void Init() override final;
+	void Shutdown() override final;
+
 	int32 GetPlayer1HiScore() const { return Player1HiScore; }
-	void SetPlayer1HiScore(int32 HiScore) { Player1HiScore = HiScore; }
+	void SetPlayer1HiScore(const int32 HiScore)
+	{
+		if (HiScore < Player1HiScore)
+			return;
+
+		Player1HiScore = HiScore;
+	}
+
+	int32 GetPlayer1CurrentScore() const { return Player1CurrentScore; }
+	void SetPlayer1CurrentScore(const int32 Score) { Player1CurrentScore = Score; }
+	void ResetPlayer1CurrentScore() { Player1CurrentScore = 0; }
 
 	int32 GetPlayer1CurrentLevel() const { return Player1CurrentLevel; }
-	void SetPlayer1CurrentLevel(int32 Level) { Player1CurrentLevel = Level; }
+	void SetPlayer1CurrentLevel(const int32 Level) { Player1CurrentLevel = Level; }
+	void AdvancePlayer1ToNextLevel() { ++Player1CurrentLevel; }
+	void ResetPlayer1CurrentLevel() { Player1CurrentLevel = 0; }
 
 private:
-	UPROPERTY(Category = "Properties", VisibleAnywhere, meta = (AllowPrivateAccess = true))
-	int32 Player1HiScore = -1;
+	UFUNCTION()
+	bool LoadGameData();
 
-	UPROPERTY(Category = "Properties", VisibleAnywhere, meta = (AllowPrivateAccess = true))
-	int32 Player1CurrentLevel = 1;
+	UFUNCTION()
+	bool SaveGameData();
+
+private:
+	UPROPERTY(Category = "Properties", EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	int32 Player1CurrentScore = 0;
+
+	UPROPERTY(Category = "Properties", EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	int32 Player1HiScore = 0;
+
+	UPROPERTY(Category = "Properties", EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+	int32 Player1CurrentLevel = 0;
 	
 };
